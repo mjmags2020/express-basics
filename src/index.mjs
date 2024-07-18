0f084cf2949237d08c6d617b9ff1fa692a20b654
Mark <mjmags2020@gmail.com>
2024-07-18 09:03:53 +0800

    API Methods = DELETE, PUT, PATCH, POST

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,10 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3002;
 
-app.get("/", (request, response) => {
-  response.status(201).send({ msg: "hello" });
-});
 const mockUserData = [
   {
     id: 1,
@@ -17,6 +15,12 @@ const mockUserData = [
     username: "mjm2",
   },
 ];
+
+// GET
+app.get("/", (request, response) => {
+  response.status(201).send({ msg: "hello" });
+});
+
 app.get("/api/users", (request, response) => {
   console.log(request.query);
   const {
@@ -27,9 +31,10 @@ app.get("/api/users", (request, response) => {
     return response
       .status(201)
       .send(mockUserData.find((user) => user.username === username));
-      
+
   response.status(201).send(mockUserData);
 });
+
 app.get("/api/users/:id", (request, response) => {
   const id = request.params.id;
   if (isNaN(id))
@@ -55,6 +60,62 @@ app.get("/api/products", (request, response) => {
       price: 31,
     },
   ]);
+});
+
+// POST
+
+app.post("/api/users", (request, response) => {
+  console.log(request.body);
+  const newUser = {
+    id: mockUserData[mockUserData.length - 1].id + 1,
+    ...request.body,
+  };
+  mockUserData.push(newUser);
+  return response.send({ msg: mockUserData });
+});
+
+// PUT
+
+app.put("/api/users/:id", (request, response) => {
+  const id = request.params.id;
+  if (isNaN(id))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+
+  const findIndex = mockUserData.findIndex((user) => user.id === +id);
+  if (findIndex === -1) return response.sendStatus(404);
+
+  mockUserData[findIndex] = { id: +id, ...request.body };
+
+  response.status(201).send(mockUserData);
+});
+
+// PATCH
+app.patch("/api/users/:id", (request, response) => {
+  const id = request.params.id;
+  if (isNaN(id))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+
+  const findIndex = mockUserData.findIndex((user) => user.id === +id);
+  if (findIndex === -1) return response.sendStatus(404);
+
+  mockUserData[findIndex] = { ...mockUserData[findIndex], ...request.body };
+
+  response.status(201).send(mockUserData);
+});
+
+// DELETE
+
+app.delete("/api/users/:id", (request, response) => {
+  const id = request.params.id;
+  if (isNaN(id))
+    return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+
+  const findIndex = mockUserData.findIndex((user) => user.id === +id);
+  if (findIndex === -1) return response.sendStatus(404);
+
+  mockUserData.splice(findIndex, 1);
+
+  response.status(201).send(mockUserData);
 });
 
 app.listen(PORT, () => {
