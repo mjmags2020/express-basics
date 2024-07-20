448da0828afe7a24635a8cce0edbded100a60a08
Mark <mjmags2020@gmail.com>
2024-07-20 23:01:36 +0800

    cookies and parser

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { response } from "express";
 import { loggingMiddleware } from "./utils/middlewares.mjs";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 3002;
 
 const app = express();
+app.use(cookieParser("helloworld"));
 app.use(express.json());
 
 app.use(loggingMiddleware);
@@ -12,4 +14,9 @@ app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.cookie("hello", "world", { maxAge: 10000, signed: true });
+  response.status(201).send({ msg: "Hi" });
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -2,18 +2,24 @@ import { Router } from "express";
 
 const router = Router();
 router.get("/api/products", (request, response) => {
-  response.status(201).send([
-    {
-      id: 1,
-      username: "chicken",
-      price: 12,
-    },
-    {
-      id: 2,
-      products: "vegetable",
-      price: 31,
-    },
-  ]);
+  console.log(request.headers.cookie);
+  console.log(request.signedCookies);
+  if (request.signedCookies.hello && request.signedCookies.hello === "world") {
+    return response.status(201).send([
+      {
+        id: 1,
+        username: "chicken",
+        price: 12,
+      },
+      {
+        id: 2,
+        products: "vegetable",
+        price: 31,
+      },
+    ]);
+  }
+
+  return response.status(403).send({ msg: "Need Correct Cookie." });
 });
 
 export default router;
